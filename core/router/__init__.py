7e103f091e55e5d53494e499340255bc7d342c56
DarcJC <me@darc.pro>
2021-11-14 01:19:06 +0800

    feat. add roll to router list

--- core/router/__init__.py
+++ core/router/__init__.py
@@ -4,9 +4,10 @@ from fastapi import APIRouter
 
 from core.router.manage import router as manage
 from core.router.user import router as user
+from core.router.roll import router as roll
 
 router_list: List[APIRouter] = [
-    manage, user
+    manage, user, roll,
 ]
 
 __all__ = ('router_list', )
